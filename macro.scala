ece9186b30a0b80f6fb252d3d22551d04ffa6272
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2012-11-29 16:04:01 +0100

    addition works

--- macro.scala
+++ macro.scala
@@ -1,37 +1,40 @@
 import scala.reflect.runtime.universe.{ WeakTypeTag, Expr }
 
-case class MyNumber[T: WeakTypeTag](val num: Number[T]) {
-  //def my_add(n: Number[T], that: Number[T]): Number[T] =
-  //  macro NumberImpl.addition_impl[T]
+class MyMeasuredNumber[T: WeakTypeTag](val num: Int) extends MeasuredNumber[T](num) {
+  //def my_add(n: MeasuredNumber[T], that: MeasuredNumber[T]): MeasuredNumber[T] =
+  //  macro MeasuredNumberImpl.addition_impl[T]
 
-  def +[U](that: Number[U])(implicit tag: WeakTypeTag[U]) = macro NumberImpl.addition_impl[U]
+  def +[U](that: MeasuredNumber[U])(implicit tag: WeakTypeTag[U]): MeasuredNumber[T] = macro MeasuredNumberImpl.addition_impl[U]
 
 /*
-  def my_multiply[U: WeakTypeTag](n: Number[T], that: Number[U]) =
-    macro NumberImpl.multiplication_impl[T, U]
+  def my_multiply[U: WeakTypeTag](n: MeasuredNumber[T], that: MeasuredNumber[U]) =
+    macro MeasuredNumberImpl.multiplication_impl[T, U] 
 
-  def *[U: WeakTypeTag](that: Number[U]) =
+  def *[U: WeakTypeTag](that: MeasuredNumber[U]) =
     my_multiply(num, that)*/
 }
 
 object Main extends App {
-  implicit def num2mynum[T: WeakTypeTag](n: Number[T]) = MyNumber[T](n)
+  implicit def num2mynum[T: WeakTypeTag](n: MeasuredNumber[T]) = new MyMeasuredNumber[T](n.n)
 
-  /*def add[T: WeakTypeTag](n: Number[T], that: Number[T]): Number[T] =
-    macro NumberImpl.addition_impl[T]*/
+  /*def add[T: WeakTypeTag](n: MeasuredNumber[T], that: MeasuredNumber[T]): MeasuredNumber[T] =
+    macro MeasuredNumberImpl.addition_impl[T]*/
 
-  val a = Number[Meter](2)
-  val b = Number[Meter](3)
-  val c = Number[Second](4)
-  val d = Number[Times[Meter, Second]](5)
-  val e = Number[Divide[Times[Second, Meter], Second]](10)
+  val a = new MeasuredNumber[Meter](2)
+  val b = new MeasuredNumber[Meter](3)
+  val c = new MeasuredNumber[Second](4)
+  val d = new MeasuredNumber[Times[Meter, Second]](5)
+  val e = new MeasuredNumber[Divide[Times[Second, Meter], Second]](10)
 
   println(a)
   println(e)
 
   println(a + b)
-  println(d + e)
-  (Number[Second](1) + Number[Second](2))
+
+  // type error
+  //println(d + e)
+
+  println(new MeasuredNumber[Second](1) + new MeasuredNumber[Second](2))
   //println(b * c)
   //println(d + (b * c))
 }
--- macros/macro_impl.scala
+++ macros/macro_impl.scala
@@ -10,27 +10,55 @@ abstract class Second extends Measure
 abstract class Divide[N, M] extends Measure
 abstract class Times[N, M] extends Measure
 
-case class Number[T: WeakTypeTag](n: Int) {
+class MeasuredNumber[T: WeakTypeTag](val n: Int) {
   override def toString = paramInfo
 
-  def paramInfo(implicit tag: WeakTypeTag[Number[T]]) = {
+  def paramInfo(implicit tag: WeakTypeTag[MeasuredNumber[T]]) = {
     val targs = tag.tpe match { case TypeRef(_, _, args) => args }
     s"type of $n has type arguments $targs"
   }
 }
 
-object NumberImpl {
-  def addition_impl[U: c.WeakTypeTag](c: Context)
-    (that: c.Expr[Number[U]])
-    (tag: c.Expr[WeakTypeTag[U]]) = {
+abstract class Typename
+case class SimpleType(name: String) extends Typename
+case class GenericType(name: String, param: Typename) extends Typename
+case class GenericType2(name: String, param1: Typename, param2: Typename) extends Typename
 
-    //println(c.prefix)
+import scala.util.parsing.combinator._
+
+object TypeParser extends JavaTokenParsers {
+  def parse(in: String) = parseAll(typename, in) match {
+    case Success(r, _) => r
+  }
+
+  // grammar
+  def typename: Parser[Typename] = (
+      ident~"["~typename~","~typename~"]" ^^ { case n~"["~t~","~u~"]" => GenericType2(n, t, u) }
+    | ident~"["~typename~"]" ^^ { case n~"["~t~"]" => GenericType(n, t) }
+    | ident ^^ (n => SimpleType(n))
+    )
+}
+
+object MeasuredNumberImpl {
+  def addition_impl[T: c.WeakTypeTag](c: Context)
+    (that: c.Expr[MeasuredNumber[T]])
+    (tag: c.Expr[WeakTypeTag[T]]) = {
 
     import c.universe._
+    
 
-    val targs = c.prefix.actualType
-    val targs2 = that.actualType
-    println(s"a: $targs, b: $targs2")
+    val typeA = TypeParser.parse(c.prefix.actualType.toString) match {
+      case GenericType(_, param) => param
+    }
+
+    val typeB = TypeParser.parse(that.actualType.toString) match {
+      case GenericType(_, param) => param
+    }
+
+    println(s"$typeA -- $typeB")
+
+    if(typeA != typeB)
+      throw new Exception(s"type error, $typeA != $typeB")
 
     val evals = ListBuffer[ValDef]()
     def precompute(value: Tree, tpe: Type): Ident = {
@@ -39,17 +67,36 @@ object NumberImpl {
       Ident(freshName)
     }
 
-    val a = precompute(that.tree, typeOf[Number[_]])
-    val b = precompute(that.tree, typeOf[Number[_]])
+    val a = precompute(that.tree, that.actualType)
+    val b = precompute(c.prefix.tree, that.actualType)
 
-    val stats = reify(Number[U](c.Expr[Number[U]](a).splice.n +
-      c.Expr[Number[U]](b).splice.n)).tree
-    //println(showRaw(stats))
-    c.Expr[Number[U]](Block(evals.toList, stats))
+
+    //println(c.prefix)
+
+    //println(showRaw(evals(0)))
+
+    /*val stats = Apply(
+      Apply(
+        TypeApply(Select(Ident(newTypeName("MeasuredNumber")), 
+                         newTermName("apply")), 
+                  List(TypeTree())), 
+        List(
+          Apply(Select(Select(Ident(newTermName("eval$1")), 
+                              newTermName("n")), 
+                      newTermName("$plus")), 
+                List(Select(Select(Ident(newTermName("eval$2")),
+                              newTermName("num")), 
+                              newTermName("n")))))), 
+      List(Select(This(newTypeName("Predef")), newTermName("implicitly"))))*/
+
+    val stats = reify(new MeasuredNumber[T](c.Expr[MeasuredNumber[T]](a).splice.n +
+      c.Expr[MeasuredNumber[T]](b).splice.n)).tree 
+    println(showRaw(stats))
+    c.Expr[MeasuredNumber[T]](Block(evals.toList, stats))
   }
 
   def multiplication_impl[T: c.WeakTypeTag, U: c.WeakTypeTag]
-    (c: Context)(n: c.Expr[Number[T]], that: c.Expr[Number[U]])
+    (c: Context)(n: c.Expr[MeasuredNumber[T]], that: c.Expr[MeasuredNumber[U]])
     (tag: c.Expr[WeakTypeTag[U]]):
       c.Expr[Any] = {
 
@@ -64,11 +111,11 @@ object NumberImpl {
       Ident(freshName)
     }
 
-    val a = precompute(n.tree, typeOf[Number[_]])
-    val b = precompute(that.tree, typeOf[Number[_]])
+    val a = precompute(n.tree, typeOf[MeasuredNumber[_]])
+    val b = precompute(that.tree, typeOf[MeasuredNumber[_]])
 
-    val stats = reify(Number[Times[T, U]](c.Expr[Number[T]](a).splice.n *
-      c.Expr[Number[T]](b).splice.n)).tree
-    c.Expr[Number[Times[T, U]]](Block(evals.toList, stats))
+    val stats = reify(new MeasuredNumber[Times[T, U]](c.Expr[MeasuredNumber[T]](a).splice.n *
+      c.Expr[MeasuredNumber[T]](b).splice.n)).tree
+    c.Expr[MeasuredNumber[Times[T, U]]](Block(evals.toList, stats))
   }
 }
