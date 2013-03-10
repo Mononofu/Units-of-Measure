d677a1f2d061f8e575f3418d6fcebea71a269085
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2013-03-10 18:12:17 +0100

    intro text about macros

--- core/src/test/scala/Measure.scala
+++ core/src/test/scala/Measure.scala
@@ -13,17 +13,17 @@ case class Complex(r: Double, i: Double)
 
 class ComplexNumeric extends Numeric[Complex] {
    def plus(x: Complex, y: Complex) = Complex(x.r + y.r, x.i + y.i)
-   def fromInt(x: Int): macros.Complex = Complex(x, 0)
-   def minus(x: macros.Complex,y: macros.Complex): macros.Complex = ???
-   def negate(x: macros.Complex): macros.Complex = ???
-   def times(x: macros.Complex,y: macros.Complex): macros.Complex = ???
-   def toDouble(x: macros.Complex): Double = x.r.toDouble
-   def toFloat(x: macros.Complex): Float = x.r.toFloat
-   def toInt(x: macros.Complex): Int = x.r.toInt
-   def toLong(x: macros.Complex): Long = x.r.toLong
+   def fromInt(x: Int): Complex = Complex(x, 0)
+   def minus(x: Complex,y: Complex): Complex = ???
+   def negate(x: Complex): Complex = ???
+   def times(x: Complex,y: Complex): Complex = ???
+   def toDouble(x: Complex): Double = x.r.toDouble
+   def toFloat(x: Complex): Float = x.r.toFloat
+   def toInt(x: Complex): Int = x.r.toInt
+   def toLong(x: Complex): Long = x.r.toLong
 
    // Members declared in scala.math.Ordering
-   def compare(x: macros.Complex,y: macros.Complex): Int = ???
+   def compare(x: Complex,y: Complex): Int = ???
 }
 
 
